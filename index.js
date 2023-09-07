d0314375cf784ddcaf9e26d0e7eaf8a9bed5130c
JosephThuku <jthuku490@gmail.com>
2023-09-07 12:32:33 +0300

    updated the github url as well as the url query

--- index.js
+++ index.js
@@ -1,26 +1,33 @@
 const express = require('express');
 const app = express();
-// get current day and time in utc
-const date = new Date();
-const current_day = date.getDay();
-const current_time = date.getTime();
-github_file_url = "https://githunfileurl.com";
-github_repo = "htttps example.com";
-
+const github_file_url = "https://github.com/JosephThuku/stageone/blob/main/index.js";
+const github_repo = "https://github.com/JosephThuku/stageone";
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// routes for handling get request with query parameters slack_name and track
-app.get('/api/:slack_name/:track', (req, res) => {
-    slackname = req.params.slack_name;
-    track = req.params.track;
-    if (!slackname, track || !track){
+app.get('/', (req, res) => {
+    res.send('Hello there please pass slackname and track on the url get request example:');
+});
+
+// Route for handling GET requests with query parameters slack_name and track
+app.get('/api', (req, res) => {
+    const slackname = req.query.slack_name;
+    const track = req.query.track;
+    
+    if (!slackname || !track) {
         return res.status(400).send('Both slack and track parameters are required');
     }
-    // return response if user perovided info
+
+    // Calculate current day and UTC time for each request
+    // Calculate the current day of the week as a string (e.g., "Monday")
+    const daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    const date = new Date();
+    const currentDay = daysOfWeek[date.getDay()];
+    const current_time = date.getTime();
+
     res.json({
         "slack_name": slackname,
-        "curret_day": current_day,
+        "current_day": currentDay,
         "utc_time": current_time,
         "track": track,
         "github_file_url": github_file_url,
@@ -28,5 +35,6 @@ app.get('/api/:slack_name/:track', (req, res) => {
         "status_code": 200
     });
 });
-const port = process.env.PORT || 3000;
-app.listen(port,()=> console.log(`listenning on port ${port}`));
+
+const port = process.env.PORT || 3030;
+app.listen(port, () => console.log(`Listening on port ${port}`));
